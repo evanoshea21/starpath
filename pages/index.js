5f4aae1415082ef3fdf694902ab40512dd59cd2d
Evan OShea <evanoshea21@gmail.com>
2023-05-14 19:22:28 -0700

    startup proj

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,39 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import Layout from '../globals/Layout.js';
-
-const inter = Inter({ subsets: ['latin'] })
-
-const pageCenter = {
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  height: '120vh'
-};
+import classes from '../styles/Home.module.css';
+import React from 'react';
+import $ from 'jquery'
 
 export default function Home() {
+
+  React.useEffect(() => {
+    $('#titles').css('opacity','1');
+    $('#img').css('opacity','.3');
+    $('#logo, #btn').css('opacity','1');
+
+
+  },[]);
+
+
+
+
   return (
     <>
-    <Layout>
-      <div style={pageCenter}>
-        <h1>This is my home page</h1>
+    <div className={classes.main}>
+
+      <div className={classes.nav}>
+        <h1 id='logo' className={classes.logo}>Starship<br/>Robotics</h1>
+
+        <div id='btn' className={classes.btnBox}>
+          <button className={classes.contactBtn} >Contact</button>
+        </div>
       </div>
-    </Layout>
+
+      <div id='titles' className={classes.titles}>
+        <h1 className={classes.title} >Making Life Multi-Planetary</h1>
+        <h2 className={classes.subtitle} >We are a company that... fill this section in with your own subheader if you want to; filling up words here.</h2>
+      </div>
+
+      <img id='img' src='/stars2.jpg' draggable="false"></img>
+    </div>
     </>
 
   )
--- server/routes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const Users = require('./controllers/Users.js');
-const Trades = require('./controllers/Trades.js');
-
-router.get('/users', Users.getAllUsers);
-router.post('/trades', Trades.getTrades);
-
-module.exports = router;
