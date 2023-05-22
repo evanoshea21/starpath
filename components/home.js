bf6d1fc6512677471a842e73e57daa5d597e4e9c
Evan OShea <evanoshea21@gmail.com>
2023-05-21 20:50:17 -0700

    fonts2

--- components/home.js
+++ components/home.js
@@ -3,12 +3,12 @@ import classes from '../styles/Home.module.css';
 import $ from 'jquery'
 
 const Home = () => {
+  let font = [1,2,3,4,5,6,7];
+  const [i, setI] = React.useState(6);
 
-  const [i, setI] = React.useState(0);
-
-  React.useEffect(() => {
-    console.log('i', i)
-  },[i]);
+  // React.useEffect(() => {
+  //   console.log('i', i)
+  // },[i]);
 
   return (
     <>
@@ -16,7 +16,7 @@ const Home = () => {
     onClick={() => setI(p => (p + 1) % 7)}
     >
     <h1 style={{
-      fontFamily: `var(--font${i})`
+      fontFamily: `var(--font${font[i]})`
     }} className={classes.title} >Making Life<br/><span className={classes.gradientText} >Multi-Planetary</span><span style={{fontSize: '.8rem'}}>font:{i}</span> </h1>
     <h2 className={classes.subtitle} >Producing starship-scale propellant in space to enable civilizations on the Moon, Mars, and beyond.</h2>
     </div>
